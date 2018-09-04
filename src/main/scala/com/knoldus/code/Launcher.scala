43bf04ffcbde1d678495cf1952f9cffcc16dfb7a
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-09-04 22:26:45 +0530

    add example 3

--- src/main/scala/com/knoldus/code/Launcher.scala
+++ src/main/scala/com/knoldus/code/Launcher.scala
@@ -2,4 +2,23 @@ package com.knoldus.code
 
 object Launcher extends App {
 
+    def f(value: Int): (Int, String) = {
+        (value + 57, s"Execute function f with $value value.")
+    }
+
+    def g(value: Int): (Int, String) = {
+        (value + 26, s"Execute function g with $value value.")
+    }
+
+    val (resultG, logG) = g(3)
+    val (resultF, logF) = f(resultG)
+
+    println(s"Result: $resultF")
+
+    val logs =
+        s"""Logs ...
+           |$logG
+           |$logF
+        """.stripMargin
+    println(logs)
 }
