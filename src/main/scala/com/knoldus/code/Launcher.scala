3aed8896f48fa5b21ea47e4b5ed66b4e38775990
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-09-04 22:25:34 +0530

    add example 2

--- src/main/scala/com/knoldus/code/Launcher.scala
+++ src/main/scala/com/knoldus/code/Launcher.scala
@@ -2,4 +2,16 @@ package com.knoldus.code
 
 object Launcher extends App {
 
+    val f: Int => Int = value => {
+        println(s"Execute function f with $value value.")
+        value + 57
+    }
+
+    val g: Int => Int = value => {
+        println(s"Execute function g with $value value.")
+        value + 26
+    }
+
+    val result = (g andThen f)(3)
+    println(s"Results $result")
 }
