fde98ad8e4189fb8f2f0adfc36bc0a524571fd89
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-09-04 23:00:55 +0530

    add example 9

--- src/main/scala/com/knoldus/code/Launcher.scala
+++ src/main/scala/com/knoldus/code/Launcher.scala
@@ -1,5 +1,29 @@
 package com.knoldus.code
 
+import java.util.UUID
+
 object Launcher extends App {
 
+    case class User(id: UUID, email: String)
+    case class Address(id: Int, city: String, country: String)
+
+    def findUsers: List[Option[User]] = {
+        // perform async database operations and return user
+        List(Some(User(UUID.randomUUID(), "harmeet@knoldus.com")))
+    }
+
+    def getAddress(user: User): List[Option[Address]] = {
+        // perform async database operations and return user address
+        List(Some(Address(13, "Moga", "India")))
+    }
+
+    val result = for {
+        userOption      <- findUsers
+        addressOption   <- userOption match {
+                                case Some(user) => getAddress(user)
+                                case None => List(None)
+                            }
+    } yield addressOption
+
+    println(s"Result: $result")
 }
