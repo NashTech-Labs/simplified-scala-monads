6259e8a46386812cdf2a1aafeeb79123e1c4361d
Harmeet Singh(Taara) <harmeetsingh.0013@gmail.com>
2018-09-04 22:28:04 +0530

    add example 4

--- src/main/scala/com/knoldus/code/Launcher.scala
+++ src/main/scala/com/knoldus/code/Launcher.scala
@@ -2,4 +2,18 @@ package com.knoldus.code
 
 object Launcher extends App {
 
+    val result1 = for {
+        a <- 1 to 5
+        b <- 1 to 5
+    } yield (a + b)
+
+    println(result1)
+
+    val result2 = 1 to 5 flatMap { a =>
+        1 to 5 map(b => a + b)
+    }
+
+    println(result2)
+
+    assert(result1 == result2)
 }
